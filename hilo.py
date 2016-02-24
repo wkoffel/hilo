f207fc9d8672dd4b2e7eca90aeb4f480c990adc4
Will Koffel <will@koffel.org>
2016-02-24 03:03:53 +0000

    basic hi/lo forecast working on pi LCD

--- hilo.py
+++ hilo.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Example using a character LCD connected to a Raspberry Pi or BeagleBone Black.
+
+# first load environment files for necessary private variables
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# now import everything else
 import time
-
+import datetime
 import Adafruit_CharLCD as LCD
-
 import RPi.GPIO as GPIO
 import forecastio
 
-forecastio_api_key = ""
+forecastio_api_key = os.environ.get("FORECAST_IO_API_KEY")
 
 # For home in Sudbury
 lat = 42.3664429
@@ -30,60 +37,49 @@ lcd_rows    = 2
 # Initialize the LCD using the pins above.
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, 
 			   lcd_columns, lcd_rows, lcd_backlight, invert_polarity=False)
-
-
-forecast = forecastio.load_forecast(forecastio_api_key, lat, lng)
-byDay = forecast.daily()
-print byDay.summary
-print byDay.icon
-
-for dailyData in byDay.data:
-    print dailyData.time.strftime('%l:%M%p %Z on %b %d, %Y')
-    print dailyData.summary
-    print dailyData.temperatureMin
-    print dailyData.temperatureMax
-    print "\n"
-
-
-
-message = "Hello Will\nReady To Serve"
-
-lcd.set_backlight(1)
-lcd.show_cursor(True)
-lcd.blink(True)
-lcd.message(message)
-time.sleep(5.0)
-lcd.blink(False)
-lcd.show_cursor(False)
-lcd.clear()
 lcd.set_backlight(0)
+lcd.clear()
+
+# pin to actually fetch forecast and display
+forecast_pin = 12
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(forecast_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
-# Stop blinking and showing cursor.
-#lcd.show_cursor(False)
-#lcd.blink(False)
+# Now the actual work, doing the forecast
+def show_forecast():
+    dayToShow = 0
+    if(datetime.datetime.now().hour > 17):
+        dayToShow = 1
+    
+    lcd.set_backlight(1)
+    lcd.show_cursor(True)
+    lcd.blink(True)
+    lcd.message("Fetching\nForecast")
+    time.sleep(1.0) # Sleep a second to build suspense, API is fast!
+    
+    forecast = forecastio.load_forecast(forecastio_api_key, lat, lng)
+    current = forecast.currently()
+    temp = str(int(round(current.temperature)))
+    byDay = forecast.daily()
+    today = byDay.data[dayToShow]
+    summary = today.summary
+    line1 = today.time.strftime('%A')
+    hi = str(int(round(today.temperatureMax)))
+    lo = str(int(round(today.temperatureMin)))
+    line2 = temp + "F L:" +lo + " H:" + hi
 
-# Demo scrolling message right/left.
-#lcd.clear()
-#message = 'Scroll'
-#lcd.message(message)
-#print range(lcd_columns-len(message))
-#for i in range(lcd_columns-len(message)):
-#	time.sleep(0.5)
-#	lcd.move_right()
-#for i in range(lcd_columns-len(message)):
-#	time.sleep(0.5)
-#	lcd.move_left()
+    lcd.clear()
+    lcd.message(line1 + "\n" + line2)
+    lcd.show_cursor(False)
+    lcd.blink(False)
+    time.sleep(5.0)
+    lcd.set_backlight(0)
+    lcd.clear()
 
-# Demo turning backlight off and on.
-#lcd.clear()
-#lcd.message('Flash backlight\nin 5 seconds...')
-#time.sleep(5.0)
-# Turn backlight off.
-#lcd.set_backlight(0)
-#time.sleep(2.0)
-# Change message.
-#lcd.clear()
-#lcd.message('Goodbye!')
-# Turn backlight on.
-#lcd.set_backlight(1)
+# And we trigger it from a simple button press
+while True:
+    input_state = GPIO.input(forecast_pin)
+    if input_state == False:
+        show_forecast()
---
+++ run.sh
@@ -0,0 +1,1 @@
+TZ='America/New_York' PYTHONIOENCODING=UTF-8 python ./hilo.py
